fd10f5edc9e180e97d2d58ced9307613e13206fc
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-11 01:36:42 -0300

    chore: refactoring all files and start a new project

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-export const Home = () => {
-    return (
-        <div className="l-home">
-            Teste para funcionamento de CRUD
-        </div>
-    )
-}
--- frontend/src/pages/User.jsx
+++
@@ -1,7 +0,0 @@
-export const User = () => {
-    return (
-        <div className="l-user">
-            Exibição da lista e formulário de CRUD
-        </div>
-    )
-}
