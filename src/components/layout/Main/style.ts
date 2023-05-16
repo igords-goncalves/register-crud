8d4c459f543937b58d6f8906387a68715299af82
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-16 11:29:10 -0300

    style: add style in template components

---
+++ src/components/layout/Main/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.div`
+  height: 100vh;
+  background-color: #fff;
+`;
---
+++ src/components/layout/SideBar/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const SideBarWrapper = styled.div`
+  height: 100vh;
+  background-color: #e2f8d8;
+`;
